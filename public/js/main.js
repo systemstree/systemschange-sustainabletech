e945b5636cf335c7a17fe0b9d4a8b5753d33b2b1
systemstree <siddhesh1981@live.com>
2025-08-27 19:13:11 +0530

    Changed file name from deploy to hugo.yml

--- public/js/main.js
+++ public/js/main.js
@@ -6410,7 +6410,7 @@ limitations under the License.
           }
 
           var query = $(this).val();
-          var searchPage = 'http://localhost:1313/search/?q=' + query;
+          var searchPage = 'http://localhost:1313/systemstree/systemschange-sustainabletech/search/?q=' + query;
           document.location = searchPage;
 
           return false;
